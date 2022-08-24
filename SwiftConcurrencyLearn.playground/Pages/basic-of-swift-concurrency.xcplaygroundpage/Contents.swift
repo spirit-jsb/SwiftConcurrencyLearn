4d1cdd63bc051597ad55c6633df12966dfa6c2e4
max <sibo_jian_29903549@163.com>
2022-08-24 15:30:13 +0800

    update example code

--- SwiftConcurrencyLearn.playground/Pages/basic-of-swift-concurrency.xcplaygroundpage/Contents.swift
+++ SwiftConcurrencyLearn.playground/Pages/basic-of-swift-concurrency.xcplaygroundpage/Contents.swift
@@ -10,12 +10,12 @@ import UIKit
 /// * 异步 (asynchronous)
 ///
 /// 同步操作意味着在操作完成之前，运行该操作的线程都将被占用，直到函数最终被抛出或返回
-example("synchronous appending value to string") {
-  var results: [String] = []
-  
-  func addAppending(_ value: String, to string: String) {
-    results.append(value.appending(string))
-  }
+///
+/// synchronous appending value to string
+var results: [String] = []
+
+func addAppending(_ value: String, to string: String) {
+  results.append(value.appending(string))
 }
 /// ──────────┬────────────┬┬─────────────┬┬─────────────┬──────────
 ///  Thread   │addAppending││Other Actions││Other Actions│
@@ -25,12 +25,12 @@ example("synchronous appending value to string") {
 /// 假设我们希望用户界面以每秒60帧的速率运行，那么主线程中每两次绘制之间所能允许的处理时间最多只有 16ms (1/60s)
 /// 当主线程中要同步处理的操作耗时过长的话，主线程将被阻塞，既无法接受用户输入、也无法向 GPU 提交绘制 UI 的请求
 /// 这种 “长耗时” 的操作是常见的：例如从网络请求中或许数据、从磁盘中加载一个大文件、进行某些复杂的加解密运算等等
-example("synchronous load signature from remote (blocking actions)") {
+///
+/// synchronous load signature from remote (blocking actions)
+func loadSignature() throws -> String? {
   // 从网络读取一个字符串
-  func loadSignature() throws -> String? {
-    let data = try Data(contentsOf: URL(string: "https://example.com")!)
-    return String(data: data, encoding: .utf8)
-  }
+  let data = try Data(contentsOf: URL(string: "https://example.com")!)
+  return String(data: data, encoding: .utf8)
 }
 ///                │            │         │
 ///                │◄───16ms───►◄─UI Lags─►
@@ -42,19 +42,19 @@ example("synchronous load signature from remote (blocking actions)") {
 /// loadSignature 最终的耗时超过 16ms，对 UI 的操作或刷新处理不得不被延后，在用户的观感上将表现为掉帧或页面卡顿
 /// Swift5.5 前，解决这个问题最常见的做法是将耗时的同步操作转为异步操作：
 /// 把实际长时间执行的任务放到另外的线程 (后台线程) 运行，在操作结束时提供运行在主线程的回调
-example("asynchronous load signature from remote") {
-  func loadSignature(_ completion: @escaping (String?, Error?) -> Void) {
-    DispatchQueue.global().async {
-      do {
-        let data = try Data(contentsOf: URL(string: "https://example.com")!)
-        DispatchQueue.main.async {
-          completion(String(data: data, encoding: .utf8), nil)
-        }
+///
+/// asynchronous load signature from remote
+func loadSignature_1(_ completion: @escaping (String?, Error?) -> Void) {
+  DispatchQueue.global().async {
+    do {
+      let data = try Data(contentsOf: URL(string: "https://example.com")!)
+      DispatchQueue.main.async {
+        completion(String(data: data, encoding: .utf8), nil)
       }
-      catch {
-        DispatchQueue.main.async {
-          completion(nil, error)
-        }
+    }
+    catch {
+      DispatchQueue.main.async {
+        completion(nil, error)
       }
     }
   }
@@ -91,80 +91,47 @@ example("asynchronous load signature from remote") {
 /// 方法被顺次调用，在同一线程中按严格的先后顺序方法，这种执行方式称之为串行 (serial)
 ///
 /// * 对于通过同步方法执行的同步操作，这些操作一定是以串行方式在同一线程中发生的
-example("synchronous serial actions") {
-  var results: [String] = []
-  
-  func addAppending(_ value: String, to string: String) {
-    results.append(value.appending(string))
-  }
-  
-  func loadSignature() throws -> String? {
-    let data = try Data(contentsOf: URL(string: "https://example.com")!)
-    return String(data: data, encoding: .utf8)
-  }
-  
-  if let signature = try? loadSignature() {
-    addAppending(signature, to: "some data")
-  }
-  
-  print(results)
+///
+/// synchronous serial actions
+if let signature = try? loadSignature() {
+  addAppending(signature, to: "some data")
 }
+
+print("synchronous serial actions results: \n\(results)\n")
 /// ───────────┬───────────────┬──────────────┬───────┬───────
 ///  Thread    │ loadSignature │ addAppending │ print │
 /// ───────────┴───────────────┴──────────────┴───────┴───────
 ///
 /// * 同步方法执行的同步操作，是串行 (serial) 的充分不必要条件，异步操作也可能会以串行的方式执行
-example("asynchronous serial actions") {
-  var results: [String] = []
-  
-  func addAppending(_ value: String, to string: String) {
-    results.append(value.appending(string))
-  }
-  
-  func loadFromDatabase(_ completion: @escaping ([String]?, Error?) -> Void) {
-    DispatchQueue.global().async {
-      sleep(2)
-      
-      DispatchQueue.main.async {
-        completion(["some data"], nil)
-      }
+///
+/// asynchronous serial actions
+func loadFromDatabase(_ completion: @escaping ([String]?, Error?) -> Void) {
+  DispatchQueue.global().async {
+    sleep(2)
+    
+    DispatchQueue.main.async {
+      completion(["some data"], nil)
     }
   }
-  
-  func loadSignature(_ completion: @escaping (String?, Error?) -> Void) {
-    DispatchQueue.global().async {
-      do {
-        let data = try Data(contentsOf: URL(string: "https://example.com")!)
-        DispatchQueue.main.async {
-          completion(String(data: data, encoding: .utf8), nil)
+}
+
+loadFromDatabase { (strings, error) in
+  if let strings = strings {
+    loadSignature_1 { (signature, error) in
+      if let signature = signature {
+        strings.forEach {
+          addAppending(signature, to: $0)
         }
+        
+        print("asynchronous serial actions results: \n\(results)\n")
       }
-      catch {
-        DispatchQueue.main.async {
-          completion(nil, error)
-        }
+      else {
+        print("Error\n")
       }
     }
   }
-  
-  loadFromDatabase { (strings, error) in
-    if let strings = strings {
-      loadSignature { (signature, error) in
-        if let signature = signature {
-          strings.forEach {
-            addAppending(signature, to: $0)
-          }
-          
-          print(results)
-        }
-        else {
-          print("Error")
-        }
-      }
-    }
-    else {
-      print("Error")
-    }
+  else {
+    print("Error\n")
   }
 }
 /// ─────────────┬┬──────────────────┬┬───────────────┬──────────────┬┬───────────────┬────────
@@ -255,11 +222,11 @@ example("asynchronous serial actions") {
 /// 1. 添加了异步函数概念
 ///
 /// 在函数声明的返回箭头前，加上 async 关键字，就可以把一个函数声明为异步函数
-asyncExample("using async function to load signature from remote") {
-  func loadSignature() async throws -> String? {
-    let (data, _) = try await URLSession.shared.data(from: URL(string: "https://example.com")!)
-    return String(data: data, encoding: .utf8)
-  }
+///
+/// using async function to load signature from remote
+func loadSignature_2() async throws -> String? {
+  let (data, _) = try await URLSession.shared.data(from: URL(string: "https://example.com")!)
+  return String(data: data, encoding: .utf8)
 }
 /// async 关键字会帮助编译器确保：
 /// * 它允许我们在函数体内部使用 await 关键字
@@ -268,31 +235,20 @@ asyncExample("using async function to load signature from remote") {
 /// await 关键字表示函数在此处可能会放弃当前线程，是程序的潜在暂停点
 /// 放弃线程，意味着异步方法可以被“暂停”，这个线程可以被用来执行其他代码。如果这个线程是主线程的话，那么页面将不会卡顿。被 await 的语句将被底层机制分配到其他合适的线程，在执行完后，之前的“暂停”将结束，异步方法从刚才的 await 语语句后开始，继续向下执行
 ///
-///
-asyncExample("using async function to asynchronous serial actions") {
-  var results: [String] = []
-  
-  func addAppending(_ value: String, to string: String) {
-    results.append(value.appending(string))
-  }
-  
-  func loadFromDatabase() async throws -> [String]? {
-    try await Task.sleep(nanoseconds: 2 * NSEC_PER_SEC)
-    return ["data1^sig", "data2^sig", "data3^sig"]
-  }
-  
-  func loadSignature() async throws -> String? {
-    let (data, _) = try await URLSession.shared.data(from: URL(string: "https://example.com")!)
-    return String(data: data, encoding: .utf8)
-  }
-  
-  if let strings = try await loadFromDatabase() {
-    if let signature = try await loadSignature() {
+/// using async function to asynchronous serial actions
+func loadFromDatabase_1() async throws -> [String]? {
+  try await Task.sleep(nanoseconds: 2 * NSEC_PER_SEC)
+  return ["data1^sig", "data2^sig", "data3^sig"]
+}
+
+Task {
+  if let strings = try await loadFromDatabase_1() {
+    if let signature = try await loadSignature_2() {
       strings.forEach {
         addAppending(signature, to: $0)
       }
       
-      print(results)
+      print("using async function to asynchronous serial actions results: \n\(results)\n")
     }
   }
 }
@@ -315,37 +271,22 @@ asyncExample("using async function to asynchronous serial actions") {
 /// * async let (异步绑定)
 /// * Task group (任务组)
 ///
-asyncExample("task function") {
-  var results: [String] = []
-  
-  func addAppending(_ value: String, to string: String) {
-    results.append(value.appending(string))
-  }
-  
-  func loadFromDatabase() async throws -> [String]? {
-    try await Task.sleep(nanoseconds: 2 * NSEC_PER_SEC)
-    return ["data1^sig", "data2^sig", "data3^sig"]
-  }
-  
-  func loadSignature() async throws -> String? {
-    let (data, _) = try await URLSession.shared.data(from: URL(string: "https://example.com")!)
-    return String(data: data, encoding: .utf8)
-  }
-  
-  func processFromScratch() async throws {
-    if let strings = try await loadFromDatabase() {
-      if let signature = try await loadSignature() {
-        strings.forEach {
-          addAppending(signature, to: $0)
-        }
+/// task function
+func processFromScratch() async throws {
+  if let strings = try await loadFromDatabase_1() {
+    if let signature = try await loadSignature_2() {
+      strings.forEach {
+        addAppending(signature, to: $0)
       }
     }
   }
-  
-  try await processFromScratch()
-  
-  print("Done:\(results)")
 }
+
+Task {
+  try await processFromScratch()
+}
+
+print("task function results: \n\(results)\n")
 /// 注意：在 processFromScratch 中的处理依然是串行 (serial) 的：对 loadFromDatabase 的 await 将使这个异步函数在此暂停，直到实际操作结束，接下来才会执行 loadSignature
 ///
 /// ┌───────────┐  ┌──────────────────┐  ┌───────────────┐    │ ┌──────────────┐
@@ -354,45 +295,44 @@ asyncExample("task function") {
 ///
 /// 当我们希望 loadFromDatabase 和 loadSignature 这两个操作可以同时进行时，就需要将任务 (Task) 以结构化 (structured) 的方式进行组织
 ///
+/// structured concurrency
 #warning("Playground can’t testing structured concurrency code")
-/*
-example("structured concurrency") {
-  var results: [String] = []
-  
-  func addAppending(_ value: String, to string: String) {
-    results.append(value.appending(string))
-  }
-  
-  func loadFromDatabase() async throws -> [String]? {
-    try await Task.sleep(nanoseconds: 2 * NSEC_PER_SEC)
-    return ["data1^sig", "data2^sig", "data3^sig"]
-  }
-  
-  func loadSignature() async throws -> String? {
-    let (data, _) = try await URLSession.shared.data(from: URL(string: "https://example.com")!)
-    return String(data: data, encoding: .utf8)
-  }
-  
-  func processFromScratch() async throws {
-    async let loadStrings = loadFromDatabase()
-    async let loadSignature = loadSignature()
-    
-    if let strings = try await loadStrings {
-      if let signature = try await loadSignature {
-        strings.forEach {
-          addAppending(signature, to: $0)
+//func processFromScratch_1() async throws {
+//  async let loadStrings = loadFromDatabase_1()
+//  async let loadSignature = loadSignature_2()
+//
+//  if let strings = try await loadStrings {
+//    if let signature = try await loadSignature {
+//      strings.forEach {
+//        addAppending(signature, to: $0)
+//      }
+//    }
+//  }
+//}
+func processFromScratch_1() async throws {
+  Task {
+    await withThrowingTaskGroup(of: Void.self) { (group) in
+      group.addTask {
+        let loadStrings = try await loadFromDatabase_1()
+        let loadSignature = try await loadSignature_2()
+        
+        if let strings = loadStrings {
+          if let signature = loadSignature {
+            strings.forEach {
+              addAppending(signature, to: $0)
+            }
+          }
         }
       }
     }
   }
-  
-  Task {
-    try await processFromScratch()
-    
-    print("Done:\(results)")
-  }
 }
- */
+
+Task {
+  try await processFromScratch_1()
+}
+
+print("structured concurrency results: \n\(results)\n")
 /// async let 被称为异步绑定，它在当前 Task 上下文中创建新的子任务，并将它用作被绑定的异步函数 (即 async let 右侧表达式) 的运行环境
 /// 与 Task.init 新建一个任务根节点不同，async let 所创建的子任务是任务树上的叶子节点，被异步绑定的操作会立即开始执行，即使在 await 之前执行就已经完成，其结果依然可以等到 await 语句时再进行求值
 ///
@@ -407,43 +347,14 @@ example("structured concurrency") {
 /// 相对于 GCD 调度的并发，基于任务的结构化并发在控制并发行为上具有明显优势
 ///
 /// 另一种创建结构化并发的方式，是使用任务组 (Task group)
-#warning("Playground can’t testing structured concurrency code")
-/*
-example("structured concurrency") {
-  var results: [String] = []
-  
-  func addAppending(_ value: String, to string: String) {
-    results.append(value.appending(string))
-  }
-  
-  func loadFromDatabase() async throws -> [String]? {
-    try await Task.sleep(nanoseconds: 2 * NSEC_PER_SEC)
-    return ["data1^sig", "data2^sig", "data3^sig"]
-  }
-  
-  func loadSignature() async throws -> String? {
-    let (data, _) = try await URLSession.shared.data(from: URL(string: "https://example.com")!)
-    return String(data: data, encoding: .utf8)
-  }
-  
-  func loadResultRemotely() async throws {
-    try await Task.sleep(nanoseconds: 2 * NSEC_PER_SEC)
-    results = ["data1^sig", "data2^sig", "data3^sig"]
-  }
-  
-  func processFromScratch() async throws {
-    async let loadStrings = loadFromDatabase()
-    async let loadSignature = loadSignature()
-    
-    if let strings = try await loadStrings {
-      if let signature = try await loadSignature {
-        strings.forEach {
-          addAppending(signature, to: $0)
-        }
-      }
-    }
-  }
-  
+///
+/// structured concurrency
+func loadResultRemotely() async throws {
+  try await Task.sleep(nanoseconds: 2 * NSEC_PER_SEC)
+  results = ["data1^sig", "data2^sig", "data3^sig"]
+}
+
+func someSyncMethod() {
   Task {
     await withThrowingTaskGroup(of: Void.self) { (group) in
       /// * group 满足 AsyncSequence 协议，可以使用 for await 的方式用类似同步循环的写法来访问异步操作的结果
@@ -453,14 +364,13 @@ example("structured concurrency") {
       }
       
       group.addTask(priority: .low) {
-        try await processFromScratch()
+        try await processFromScratch_1()
       }
     }
-    
-    print("Done:\(results)")
   }
+  
+  print("structured concurrency results: \n\(results)\n")
 }
- */
 /// 对于 processFromScratch 我们将它特别指定为 .low 的优先级，这会导致该任务在另一个低优先级线程中被调度
 ///
 ///                    ┌────────────────────────────────────────────┐
